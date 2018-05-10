f4029d55518e412d1bac14659a3aacdf56136f1e
Tomy <tomyleereyes@gmail.com>
2018-05-09 18:03:36 -0700

    experimenting

--- App.js
+++ App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      text: ''
+    }
+  }
+  changeText = () => {
+    this.setState({text: })
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+    return <View style={styles.container}>
+        <TextInput onChange={this.changeText} />
+        <Text>{this.state.text}</Text>
       </View>
-    );
   }
 }
 
