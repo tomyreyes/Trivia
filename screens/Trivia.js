e2716e4155f0e740de70ed37bdfdef49e9f4214a
Tomy <tomyleereyes@gmail.com>
2018-05-11 00:58:11 -0700

    added fetch

--- screens/Trivia.js
+++ screens/Trivia.js
@@ -2,23 +2,37 @@ import React from 'react';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default class Trivia extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      isLoading: true,
+      questions: []
+    }
+  }
   //componentWillMount
-  //API call will occur here 
-  
-
-  //function to change question 
-
-
+  componentWillMount(){
+    let { id } = this.props.navigation.state.params
+    return fetch(`https://opentdb.com/api.php?amount=10&category=${id}&type=multiple`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        // console.error(responseJson.results)
+        this.setState({
+          questions: responseJson.results,
+          isLoading: false
+        })
+  })
+  .catch(error => {
+    console.error(error)
+  })
+}
 
   render() {
-    let id = this.props.navigation.state.params.id
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>{id}</Text>
-      </View>
+    return(
+      <View><Text>Hi</Text></View>
     )
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
