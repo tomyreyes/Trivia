688c8af62157c751858b85c7e76a86cafd3c0803
Tomy <tomyleereyes@gmail.com>
2018-05-12 19:28:09 -0700

    incrementing through questions

--- screens/Trivia.js
+++ screens/Trivia.js
@@ -7,33 +7,64 @@ export default class Trivia extends React.Component {
     super();
     this.state = {
       isLoading: true,
-      questions: []
+      questions: [],
+      questionNumber: 0
     }
   }
 
-  componentDidMount(){
+  componentWillMount(){
     let { id } = this.props.navigation.state.params
 
     axios.get(`https://opentdb.com/api.php?amount=10&category=${id}`)
     .then(response => {
       this.setState({
-        questions: response.data.results
+        questions: response.data.results,
+        
       })
     })
-
   }
-  
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.questions !== this.state.questions){
+      this.setState({
+        isLoading: false,
+        questions: this.state.questions
+      })
+    }
+  }
+  changeQuestion = () =>{
+    this.setState({
+      questionNumber: this.state.questionNumber + 1
+    })
+  }
+
+  // MC ON PRESS FUNCTION
+  // HIGHLIGHT THE BUTTON, CHANGE STATE OF CHOICE
+
+  // SUBMIT ON PRESS FUNCTION
+  //CHECK IF ANSWER IS RIGHT THEN CHANGE QUESTION
+
   render(){
-    
-    let trivia = this.state.questions.map(((quest, i) =>{
-      return <Text key={i}>{quest.question}</Text>
-    }))
+    const { questions, questionNumber } = this.state
+    const triviaQuestion = questions[questionNumber]
+
+    // NEED ARRAY THAT CONTAINS MULTIPLE CHOICE 
+       
+if(this.state.isLoading) {
     return(
       <View>
-        <Text>Hey</Text>
-        {trivia}
+        <Text>Loading</Text>
       </View>
     )
+  }
+  if(this.state.isLoading === false){
+    return(
+      <View>
+      <Text>{triviaQuestion.question}</Text>
+      <Button title="Change" onPress={this.changeQuestion}/>
+      </View>
+    )
+  }
   }
 }
 const styles = StyleSheet.create({
