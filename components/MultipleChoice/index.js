4d16187c3c590a43413430335a832c728c63ed69
Tomy <tomyleereyes@gmail.com>
2018-05-21 12:47:09 -0700

    multiple choice questions rendering

--- components/MultipleChoice/index.js
+++ components/MultipleChoice/index.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react'
 import { Button, StyleSheet, Text, View} from 'react-native'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { changeQuestion } from '../../actions'
 
 class MultipleChoice extends Component {
   renderMultipleChoice(){
-    
+    const { index } = this.props.questionIndex
+    const { categoryData } = this.props.categoryData
+    const answerSet = categoryData.filter(((answers, i) => i === index)) //this will give me access to [index] answer set
+     
+    const multipleChoice = answerSet[0].incorrect_answers // I need to add the correct answer to this 
+    const correct = answerSet[0].correct_answer
+    const randomPlacement = Math.floor(Math.random() * multipleChoice.length)
+
+    multipleChoice.splice(randomPlacement, 0, correct) //adding correct answer at a random index
+    return multipleChoice.map((choice, i) => {
+      return ( <Text key={i}>{choice}</Text> 
+    )
+    })    
   }
   render(){
     return(
-      <Text>nice</Text>
+      this.renderMultipleChoice()
+      
     )
   }
 }
 
+mapStateToProps = state => {
+  return {
+    categoryData: state.categoryReducer,
+    questionIndex: state.questionReducer
+  }
+}
 
-// export default connect(mapStateToProps, mapDispatchToProps)(MultipleChoice)
-export default MultipleChoice
+mapDispatchToProps = dispatch => { 
+  return bindActionCreators({
+    changeQuestion: changeQuestion
+  },dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MultipleChoice)
--- screens/Trivia.js
+++ screens/Trivia.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
   }
 });
 
-mapStateToProps = state => { //subscribe to changes in the store 
+mapStateToProps = state => {
   return {
     categoryData: state.categoryReducer,
     questionIndex: state.questionReducer
