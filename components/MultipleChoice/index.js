b99fa8f4590754be0f8992d9b70e0ff1b48387dc
Tomy <tomyleereyes@gmail.com>
2018-05-22 17:41:39 -0700

    buttons toggling depending on if pressed or not

--- components/MultipleChoice/index.js
+++ components/MultipleChoice/index.js
@@ -5,57 +5,130 @@ import { bindActionCreators } from 'redux'
 import { changeQuestion } from '../../actions'
 
 class MultipleChoice extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
       randomPlacement: 0,
-      active: false,
+      activeA: false,
+      activeB: false,
+      activeC: false,
+      activeD: false,
       multipleChoice: []
     }
   }
 
-  _pickAnswer = () => {
+  _pickAnswerA = () => {
+    const { activeA, activeB, activeC, activeD } = this.state
+    if(activeA === true || activeB === true || activeC === true || activeD === true) {
+      this.setState({
+        activeA: false,
+        activeB: false, 
+        activeC: false, 
+        activeD: false,
+       })
+    }
     this.setState({
-      active: true
+      activeA: true
+    })
+  }
+  _pickAnswerB = () => {
+    const { activeA, activeB, activeC, activeD } = this.state
+    if (activeA === true || activeB === true || activeC === true || activeD === true) {
+      this.setState({
+        activeA: false,
+        activeB: false,
+        activeC: false,
+        activeD: false,
+      })
+    }
+    this.setState({
+      activeB: true
+    })
+  }
+  _pickAnswerC = () => {
+    const { activeA, activeB, activeC, activeD } = this.state
+    if (activeA === true || activeB === true || activeC === true || activeD === true) {
+      this.setState({
+        activeA: false,
+        activeB: false,
+        activeC: false,
+        activeD: false,
+      })
+    }
+    this.setState({
+      activeC: true
+    })
+  }
+  _pickAnswerD = () => {
+    const { activeA, activeB, activeC, activeD } = this.state
+    if (activeA === true || activeB === true || activeC === true || activeD === true) {
+      this.setState({
+        activeA: false,
+        activeB: false,
+        activeC: false,
+        activeD: false,
+      })
+    }
+    this.setState({
+      activeD: true
     })
   }
 
-  componentWillMount() { //add the logic in here 
-     
-    this.setState({
-      randomPlacement: Math.floor(Math.random() * 2),
-    },()=> {
-      const { index } = this.props.questionIndex
-      const { categoryData } = this.props.categoryData
-      const answerSet = categoryData.filter(((answers, i) => i === index)) //this will give me access to [index] answer set
-      const incorrect = answerSet[0].incorrect_answers // I need to add the correct answer to this 
-      const correct = answerSet[0].correct_answer
-      incorrect.splice(this.state.randomPlacement, 0, correct)
-      this.setState({multipleChoice: incorrect}) //this will fill the button group with correct answers each update.
-    }) 
+  componentWillMount() {
+    //add the logic in here
+    const { index } = this.props.questionIndex
+    const { categoryData } = this.props.categoryData
+
+    this.setState(
+      {
+        randomPlacement: Math.floor(Math.random() * 2)
+      },
+      () => {
+        const answerSet = categoryData.filter((answers, i) => i === index)
+        const incorrect = answerSet[0].incorrect_answers
+        const correct = answerSet[0].correct_answer
+        incorrect.splice(this.state.randomPlacement, 0, correct)
+        this.setState({ multipleChoice: incorrect })
+      }
+    )
   }
 
-  renderMultipleChoice(){
+  renderMultipleChoice() {
     const { multipleChoice } = this.state
     return (
       <View>
-        <Button title={multipleChoice[0]} onPress={this._pickAnswer} color={(this.state.active === true) ? 'black' : 'red'}></Button>
-        <Button title={multipleChoice[1]} color={'grey'}></Button>
-        <Button title={multipleChoice[2]} color={'green'}></Button>
-        <Button title={multipleChoice[3]}></Button>
+        <Button
+          title={multipleChoice[0]}
+          onPress={this._pickAnswerA}
+          color={this.state.activeA === true ? 'black' : 'red'}
+        />
+        <Button
+          title={multipleChoice[1]}
+          onPress={this._pickAnswerB}
+          color={this.state.activeB === true ? 'black' : 'grey'}
+        />
+        <Button
+          title={multipleChoice[2]}
+          onPress={this._pickAnswerC}
+          color={this.state.activeC === true ? 'black' : 'green'}
+        />
+        <Button
+          title={multipleChoice[3]}
+          onPress={this._pickAnswerD}
+          color={this.state.activeD === true ? 'black' : 'blue'}
+        />
       </View>
     )
   }
-  shouldComponentUpdate(nextState){
+  shouldComponentUpdate(nextState) {
     return this.state.multipleChoice !== nextState.multipleChoice
   }
-  
-  render(){
-    return(
-    <View>{this.state.multipleChoice.length > 0 &&
-     this.renderMultipleChoice()
-    }
-     </View>
+
+  render() {
+    return (
+      <View>
+        {this.state.multipleChoice.length > 0 && this.renderMultipleChoice()}
+      </View>
     )
   }
 }
